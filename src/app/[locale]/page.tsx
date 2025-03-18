2c31750ecfd11c5bf34f21a1f80011b5121a0dd1
blazehp <blazehunterhp@gmail.com>
2025-03-17 21:50:28 -0400

    added test model

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   const t = useTranslations('HomePage');
   return (
     <> 
-      <h3>{t('title')}</h3>
+      <div className="sketchfab-embed-wrapper"> <iframe title="Fractal Define 7 XL Tempered Glass" frameBorder="0" allowFullScreen  allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking="true" execution-while-out-of-viewport="true" execution-while-not-rendered="true" web-share="true" src="https://sketchfab.com/models/7c89802dbaa0490587ff165aace4debe/embed?autospin=1&autostart=1&transparent=1"> </iframe> <p style={{ fontSize: 13, fontWeight: 'normal', margin: 5, color: '#4A4A4A'}}> <a href="https://sketchfab.com/3d-models/fractal-define-7-xl-tempered-glass-7c89802dbaa0490587ff165aace4debe?utm_medium=embed&utm_campaign=share-popup&utm_content=7c89802dbaa0490587ff165aace4debe" target="_blank" rel="nofollow" style={{fontWeight: 'bold', color: '#1CAAD9'}}> Fractal Define 7 XL Tempered Glass </a> by <a href="https://sketchfab.com/digitalrazor3d?utm_medium=embed&utm_campaign=share-popup&utm_content=7c89802dbaa0490587ff165aace4debe" target="_blank" rel="nofollow" style={{fontWeight: 'bold', color: '#1CAAD9'}}> digitalrazor3d </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=7c89802dbaa0490587ff165aace4debe" target="_blank" rel="nofollow" style={{fontWeight: 'bold', color: '#1CAAD9'}}>Sketchfab</a></p></div>
     </>
   );
 }
