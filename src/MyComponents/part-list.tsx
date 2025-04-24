a6691fa3a10cfdcc9a3573fd8d8fe681c5f7d093
aalibrahimi <166450703+aalibrahimi@users.noreply.github.com>
2025-04-23 23:37:01 -0700

    Finished the pc part list

--- src/MyComponents/part-list.tsx
+++ src/MyComponents/part-list.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { getParts, Part, PartType } from "@/lib/types"
 
@@ -19,11 +20,11 @@ export default function PartList({ partType, onSelectPart, selectedPart, disable
             {parts.map((part) => (
                <Card
                 key={part.id}
-                className={`transition-all duration-300 hover:shadow-lg hover:shadow-purple-500/20 cursor-pointer border ${
-                    selectedPart?.id === part.id ? "border-purple-500" : "border-border hover:border-purple-500/50"
+                className={`transition-all duration-300 hover:shadow-lg bg-black hover:shadow-red-500/20 cursor-pointer border ${
+                    selectedPart?.id === part.id ? "border-red-500" : "border-border hover:border-red-500/50"
                 }`}
                >
-                 <CardContent className="p-3 flex items-center gap-3">
+                 <CardContent className="p-3 flex items-center gap-3 ">
                     <div
                         style={{ backgroundColor: part.color || "5f27cd" }}
                         className="w-12 h-12 rounded-md flex  items-center justify-center"
@@ -34,10 +35,21 @@ export default function PartList({ partType, onSelectPart, selectedPart, disable
                     <div className="flex-1">
                         <h3 className="font-medium text-sm">{part.name}</h3>
                         <p className="text-sm text-muted-foreground">{part.specs}</p>
+                       
                     </div>
 
                     <div className="flex flex-col items-end gap-2">
-                        <Badge className="outline">${part.specs}</Badge>
+                        
+                        <Badge className="bg-green-950/1 text-green-500 outline">${part.specs}</Badge>
+                        <p className="flex  justify-end">${part.price}</p>
+                        <Button 
+                        size="sm"
+                        variant={selectedPart?.id  === part.id ?  "default" : "outline"}
+                        onClick={() => onSelectPart(part)}
+                        disabled={disabled || selectedPart?.id === part.id}
+                        className="">
+                            {selectedPart?.id === part.id ? "Selected" : "Select"}
+                        </Button>
                     </div>
                 </CardContent>
 
